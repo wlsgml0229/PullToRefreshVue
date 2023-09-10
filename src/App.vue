c268215a70736ea9dda7d403cffca70a0a377f3e
wlsgml0229 <wlsgml0229@naver.com>
2023-09-10 22:36:58 +0900

    Feat: 예시화면 스타일링 추가

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,31 @@
 <template>
-  <PullToRefresh :loading="loading" @refresh="refreshHandler">
-    <div style="background: pink; height: 800px;"></div>
+  <PullToRefresh :loading="loading" @refresh="refreshHandler" :color="'#F3FDE8'">
+    <section>
+    <header>
+      <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="#F3FDE8" class="bi bi-list" viewBox="0 0 16 16">
+        <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+      </svg>
+    </header>
+      <main>
+        <h1>Welecome, Jinhee
+        <strong>Schedule</strong>
+        </h1>
+        <div class="box-wrapper">
+
+        </div>
+        <div class="box-wrapper">
+
+        </div>
+        <div class="box-wrapper">
+
+        </div>
+        <div class="box-wrapper">
+
+        </div>
+      </main>
+    </section>
   </PullToRefresh>
+
 </template>
 
 <script>
@@ -26,4 +50,45 @@ export default {
   }
 }
 </script>
+<style>
+@import "@/assets/css/reset.css";
 
+body {
+  background: #FFBFBF;
+  width: 100%;
+}
+section {
+  width: 100%;
+  height: auto;
+}
+header {
+  width: 100%;
+  height: 45px;
+  border-bottom: 2px dotted #F3FDE8;
+  color: #F3FDE8;
+  display: flex;
+  align-items: center;
+  font-weight: bold;
+  padding: 0 15px;
+}
+main {
+  padding: 40px 30px;
+}
+main > h1 {
+  font-size: 18px;
+  color: white;
+}
+main > h1 > strong {
+  font-size: 24px;
+  display: block;
+  margin-top: 10px;
+  color: #F3FDE8;
+}
+.box-wrapper {
+  margin-top: 20px;
+  width: 100%;
+  height: 150px;
+  border-radius: 15px;
+  border: 1px solid #F3FDE8;
+}
+</style>
