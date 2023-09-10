405042acfeb260d77d971bd60b14d75f3bb4ebd8
wlsgml0229 <wlsgml0229@naver.com>
2023-09-11 02:01:05 +0900

    Feat: npm 패키지화 설정 추가

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import PullToRefreshVue from "./components/PullToRefresh.vue";
+
+export function install(Vue) {
+  if (install.installed) return;
+  install.installed = true;
+  Vue.component("PullToRefreshVue", PullToRefreshVue);
+}
+
+const plugin = {
+  install,
+};
+
+let GlobalVue = null;
+if (typeof window !== "undefined") {
+  GlobalVue = window.Vue;
+} else if (typeof global !== "undefined") {
+  GlobalVue = global.Vue;
+}
+if (GlobalVue) {
+  GlobalVue.use(plugin);
+}
+
+export default PullToRefreshVue;
